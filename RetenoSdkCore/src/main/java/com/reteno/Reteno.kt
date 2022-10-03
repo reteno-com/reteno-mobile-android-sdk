7d6456620f615503681a1245225fd8a22b141bc5
dmorozov <dmytro.morozov@yalantis.net>
2022-10-03 10:13:04 +0300

    Add main class
    - Add main SDK class
    - Add init method
    
    Signed-off-by: dmorozov <dmytro.morozov@yalantis.net>

---
+++ RetenoSdkCore/src/main/java/com/reteno/Reteno.kt
@@ -0,0 +1,10 @@
+package com.reteno
+
+import android.content.Context
+
+object Reteno {
+
+    fun init(context: Context) {
+        // TODO: This should be the first method to be started from Application class
+    }
+}
