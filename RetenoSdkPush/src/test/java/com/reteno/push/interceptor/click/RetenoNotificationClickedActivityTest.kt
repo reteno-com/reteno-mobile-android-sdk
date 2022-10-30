1cb31a514f99d2110c2acd6b6e2a53879d348b89
dmorozov <dmytro.morozov@yalantis.net>
2022-10-30 11:20:34 +0200

    YSP-125: #2 Handle custom data
    - YSP-227: Unit tests
    
    Signed-off-by: dmorozov <dmytro.morozov@yalantis.net>

--- RetenoSdkPush/src/test/java/com/reteno/push/interceptor/click/RetenoNotificationClickedActivityTest.kt
+++ RetenoSdkPush/src/test/java/com/reteno/push/interceptor/click/RetenoNotificationClickedActivityTest.kt
@@ -1,6 +1,7 @@
 package com.reteno.push.interceptor.click
 
 import android.app.Application
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import com.reteno.core.RetenoApplication
@@ -11,9 +12,11 @@ import com.reteno.push.Constants
 import com.reteno.push.Util
 import com.reteno.push.base.robolectric.BaseRobolectricTest
 import io.mockk.*
+import io.mockk.impl.annotations.RelaxedMockK
 import junit.framework.Assert.assertNotNull
 import org.junit.After
 import org.junit.Assert
+import org.junit.Assert.assertEquals
 import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Test
@@ -24,10 +27,20 @@ import org.robolectric.annotation.Config
 @Config(sdk = [26])
 class RetenoNotificationClickedActivityTest : BaseRobolectricTest() {
 
+    // region constants ----------------------------------------------------------------------------
+    companion object {
+        private const val DEEPLINK_WRAPPED = "https://wrapped.com"
+        private const val DEEPLINK_UNWRAPPED = "https://unwrapped.com"
+    }
+    // endregion constants -------------------------------------------------------------------------
+
+    // region helper fields ------------------------------------------------------------------------
+    @RelaxedMockK
+    private lateinit var context: Context
+    // endregion helper fields ---------------------------------------------------------------------
 
     @Before
     fun setUp() {
-        MockKAnnotations.init(this)
         mockkObject(Util)
     }
 
@@ -81,6 +94,9 @@ class RetenoNotificationClickedActivityTest : BaseRobolectricTest() {
 
     @Test
     fun launchApp_doNotHaveDeepLinkAndExtrasIsNull() {
+        mockkObject(IntentHandler.AppLaunchIntent)
+        every { IntentHandler.AppLaunchIntent.getAppLaunchIntent(any()) } returns Intent()
+
         val extra = Bundle().apply { putString("key", "value") }
         val intent = Intent()
         intent.putExtras(extra)
@@ -94,13 +110,15 @@ class RetenoNotificationClickedActivityTest : BaseRobolectricTest() {
 
         assertNotNull(shadowIntent)
         assertTrue(activity.isFinishing)
+
+        unmockkObject(IntentHandler.AppLaunchIntent)
     }
 
     @Test
     fun launchDeepLink() {
         val deepLink = "com.reteno.example"
         val intent = Intent()
-        intent.putExtra(Constants.KEY_ES_LINK, deepLink)
+        intent.putExtra(Constants.KEY_ES_LINK_WRAPPED, deepLink)
 
         val activity = buildActivity(RetenoNotificationClickedActivity::class.java, intent)
             .create()
@@ -119,6 +137,9 @@ class RetenoNotificationClickedActivityTest : BaseRobolectricTest() {
 
     @Test
     fun launchApp_extrasIsNull() {
+        mockkObject(IntentHandler.AppLaunchIntent)
+        every { IntentHandler.AppLaunchIntent.getAppLaunchIntent(any()) } returns Intent()
+
         val activity = buildActivity(RetenoNotificationClickedActivity::class.java).create().get()
 
         val shadow = shadowOf(activity)
@@ -126,6 +147,67 @@ class RetenoNotificationClickedActivityTest : BaseRobolectricTest() {
 
         assertNotNull(intent)
         assertTrue(activity.isFinishing)
+
+        unmockkObject(IntentHandler.AppLaunchIntent)
     }
 
+    @Test
+    fun givenPushWithCustomDataNoDeeplinkReceived_whenNotificationClicked_thenCustomDataDeliveredToLaunchActivity() {
+        // Given
+        mockkObject(IntentHandler.AppLaunchIntent)
+        every { IntentHandler.AppLaunchIntent.getAppLaunchIntent(any()) } returns Intent()
+
+        val customDataKey = "customDataKey"
+        val customDataValue = "customDataValue"
+
+        val extra = Bundle().apply { putString(customDataKey, customDataValue) }
+        val intent = Intent()
+        intent.putExtras(extra)
+        justRun { context.startActivity(any()) }
+
+        // When
+        val activity = buildActivity(RetenoNotificationClickedActivity::class.java, intent).create().get()
+        val shadow = shadowOf(activity)
+        val appLaunchIntent = shadow.peekNextStartedActivity()
+
+        // Then
+        assertNotNull(appLaunchIntent)
+        assertNotNull(appLaunchIntent.extras)
+        assertTrue(appLaunchIntent.hasExtra(customDataKey))
+        assertEquals(customDataValue, appLaunchIntent.getStringExtra(customDataKey))
+
+        unmockkObject(IntentHandler.AppLaunchIntent)
+    }
+
+    @Test
+    fun givenPushWithCustomDataDeeplinkReceived_whenNotificationClicked_thenCustomDataDeliveredToLaunchActivity() {
+        // Given
+        val customDataKey = "customDataKey"
+        val customDataValue = "customDataValue"
+
+        val extra = Bundle().apply { putString(customDataKey, customDataValue) }
+        val intent = Intent().apply {
+            putExtras(extra)
+            putExtra(
+                Constants.KEY_ES_LINK_WRAPPED,
+                DEEPLINK_WRAPPED
+            )
+            putExtra(
+                Constants.KEY_ES_LINK_UNWRAPPED,
+                DEEPLINK_UNWRAPPED
+            )
+        }
+        justRun { context.startActivity(any()) }
+
+        // When
+        val activity = buildActivity(RetenoNotificationClickedActivity::class.java, intent).create().get()
+        val shadow = shadowOf(activity)
+        val appLaunchIntent = shadow.peekNextStartedActivity()
+
+        // Then
+        assertNotNull(appLaunchIntent)
+        assertNotNull(appLaunchIntent.extras)
+        assertTrue(appLaunchIntent.hasExtra(customDataKey))
+        assertEquals(customDataValue, appLaunchIntent.getStringExtra(customDataKey))
+    }
 }
