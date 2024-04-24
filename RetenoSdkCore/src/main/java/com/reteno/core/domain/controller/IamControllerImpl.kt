de4a3d4da6b51148e796d1486ab79cc7836c16e1
Michael Antiufieiev <mykhailo.antiufieiev@reteno.com>
2024-04-24 11:08:56 +0300

    [no_task] fix inapp paused bug

--- RetenoSdkCore/src/main/java/com/reteno/core/domain/controller/IamControllerImpl.kt
+++ RetenoSdkCore/src/main/java/com/reteno/core/domain/controller/IamControllerImpl.kt
@@ -23,6 +23,7 @@ import kotlinx.coroutines.Job
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.TimeoutCancellationException
 import kotlinx.coroutines.async
+import kotlinx.coroutines.channels.BufferOverflow
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharedFlow
@@ -53,7 +54,10 @@ internal class IamControllerImpl(
         private set
     private var htmlJob: Job? = null
 
-    private val _inAppMessage = MutableSharedFlow<InAppMessage>()
+    private val _inAppMessage = MutableSharedFlow<InAppMessage>(
+        extraBufferCapacity = 10,
+        onBufferOverflow = BufferOverflow.DROP_OLDEST
+    )
     private val postponedNotifications = mutableListOf<InAppMessage>()
     override val inAppMessagesFlow: SharedFlow<InAppMessage> = _inAppMessage
 
