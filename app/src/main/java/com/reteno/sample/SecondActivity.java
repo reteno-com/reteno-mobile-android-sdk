53df70c77e990d2310984e7524187f61a9a0d23f
Michael Antiufieiev <mykhailo.antiufieiev@reteno.com>
2024-07-30 14:21:03 +0300

    fix splash support feature

--- app/src/main/java/com/reteno/sample/SecondActivity.java
+++ app/src/main/java/com/reteno/sample/SecondActivity.java
@@ -6,14 +6,25 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
+import com.reteno.core.RetenoImpl;
+
 public class SecondActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        RetenoImpl.Companion.getInstance().pausePushInAppMessages(true);
         new Handler().postDelayed(() -> {
             startActivity(new Intent(this, MainActivity.class));
+            finish();
         }, 2000L);
+    }
+
+    @Override
+    protected void onStop() {
+        RetenoImpl.Companion.getInstance().pausePushInAppMessages(false);
+        super.onStop();
     }
 }
