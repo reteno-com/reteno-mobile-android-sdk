7e0c3f9c2413a718c94c8ad751999dcea6806f43
Michael Antiufieiev <mykhailo.antiufieiev@reteno.com>
2025-07-31 14:12:35 +0300

    Migrate to androidx initialization

---
+++ RetenoSdkCore/src/main/java/com/reteno/core/RetenoInitializer.kt
@@ -0,0 +1,16 @@
+package com.reteno.core
+
+import android.app.Application
+import android.content.Context
+import androidx.startup.Initializer
+
+class RetenoInitializer : Initializer<Reteno> {
+    override fun create(context: Context): Reteno {
+        Reteno.create(context.applicationContext as Application)
+        return Reteno.instance
+    }
+
+    override fun dependencies(): List<Class<out Initializer<*>>> {
+        return emptyList()
+    }
+}
